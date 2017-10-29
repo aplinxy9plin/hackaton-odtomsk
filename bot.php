a50680badd626d9514a393e146d683ce80b16a9f
AplinXy9plin <evilberry@icloud.com>
2017-10-29 14:45:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,7 +4,19 @@
 if (!isset($_REQUEST)) { 
   return; 
 } 
-
+  $data = array();                                                                    
+  $data_string = json_encode($data);                                                                                   
+   
+  $ch = curl_init('http://api.pss.tomsk.ru');                                                                      
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+      'Content-Type: application/json',                                                                                
+      'Content-Length: ' . strlen($data_string))                                                                       
+  );                                                                                                                   
+   
+  $result = curl_exec($ch);
 //Строка для подтверждения адреса сервера из настроек Callback API 
 $confirmation_token = ''; 
 
